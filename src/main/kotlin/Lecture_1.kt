c548d45e1f5912ef6eccee795c0447decd57d52a
Heavendart888 <mynameisbiswajit8@gmail.com>
2023-08-01 22:35:45 +0530

    Lecture_1

---
+++ src/main/kotlin/Lecture_1.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    println("Hello Teddy\nHello melon")
+
+    var a="Btech"
+    println(a)
+
+    var b:Float=3.4f
+    println(b)
+    println(b.javaClass.name)
+
+    // this is single line comment
+
+    /* this is multi
+    line comment
+     */
+
+    /**
+     * This is a Documentation comment.
+     */
+
+}
